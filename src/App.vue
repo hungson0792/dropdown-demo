5d31d16e8f98be5dd48954c2f427ed854c8d153c
hungson <hungson0792@gmail.com>
2021-10-07 13:43:47 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+      <List />
+  </div>
+</template>
+
+<script>
+import List from './components/List.vue'
+
+export default {
+    data(){
+      return {
+        
+      }
+    },
+    components: {
+      List
+    }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/List.vue
@@ -0,0 +1,52 @@
+<template>
+    <div>
+        <input type="text" name="" id=" " v-model="newTodo">
+        <button @click="addTodo()">Add</button>
+        <div class="List">
+             <Todo 
+             v-for="(todo,index) in todos" 
+             :key="index" 
+             :todo="todo"
+             :deleteTodo="deleteTodo"
+             :index = "index"
+             />
+        </div>
+       
+    </div>
+</template>
+
+<script>
+import Todo from './Todo.vue'
+
+export default {
+    data(){
+        return {
+            newTodo: '',
+            todos: [
+               
+            ]
+        }
+    },
+    methods:{
+        addTodo(){
+            this.todos.push({name:this.newTodo,done:false});
+            this.newTodo = '';
+        },
+        deleteTodo(index){
+            this.todos.splice(index,1)
+        }
+    },
+    components: {
+        Todo
+    }
+}
+</script>
+
+<style scoped>
+    .List{
+       font-family: Avenir, Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;        
+        color: #2c3e50;        
+    }
+</style>
